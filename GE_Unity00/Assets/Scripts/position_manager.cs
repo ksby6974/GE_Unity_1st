5f7e49279232c0ad1329a14ff96f00d27190ef90
hghg919 <71994073+ksby6974@users.noreply.github.com>
2024-08-13 15:23:35 +0900

    240813
    .

--- GE_Unity00/Assets/Scripts/position_manager.cs
+++ GE_Unity00/Assets/Scripts/position_manager.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class position_manager : MonoBehaviour
 {
     [SerializeField] Transform [] spawners;
+    [SerializeField] GameObject obj_aircraft;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Invoke("ResetPosition",5);
+        InvokeRepeating("ResetPosition", 0, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //
+    }
+
+    public void ResetPosition()
+    {
+        int random = Random.Range(0, spawners.Length);
+        Debug.Log(spawners[random].position);
+
+        obj_aircraft.transform.position = new Vector3(spawners[random].position.x, spawners[random].position.y, spawners[random].position.z);
     }
 }
