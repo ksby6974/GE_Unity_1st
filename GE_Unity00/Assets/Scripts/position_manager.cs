e88c21986a48d750bdf0e68010bad3b98717dc6a
hghg919 <71994073+ksby6974@users.noreply.github.com>
2024-08-13 14:44:58 +0900

    240813
    .

---
+++ GE_Unity00/Assets/Scripts/position_manager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class position_manager : MonoBehaviour
+{
+    [SerializeField] Transform [] spawners;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
